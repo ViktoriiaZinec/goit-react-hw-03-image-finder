6191a26e32c759d59865ffd94adf2b35d545dce5
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-06-03 20:20:24 +0300

    Update after checking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,110 @@
 import { SearchBar } from './SearchBar/SearchBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Component } from 'react';
+import { fetchPhotos } from 'servise/image-servise';
+import { Modal } from './Modal/Modal';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
 
 export class App extends Component {
   state = {
-    search: '',
+    photos: [],
+    loading: true,
+    error: false,
+    page: 1,
+    modalItem: null,
+    isLoading: false,
+    pageCount: 1,
+    isEmpty: false,
+  };
+
+  callServer(searchLine, page) {
+    this.setState(state => ({
+      ...state,
+      isLoading: true,
+      isEmpty: false,
+    }));
+
+    fetchPhotos(searchLine, page)
+      .then(result => {
+        this.setState(state => ({
+          photos: [...state.photos, ...result.hits],
+          loading: false,
+          error: false,
+          isLoading: false,
+          page: page,
+          pageCount: Math.ceil(result.total / 12),
+          isEmpty: result.total === 0,
+        }));
+      })
+      .catch(error => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  clearPhotos() {
+    this.setState(state => ({
+      photos: [],
+      loading: false,
+      error: false,
+      page: 1,
+      pageCount: 1,
+      isEmpty: false,
+    }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) {
+      this.clearPhotos();
+      this.callServer(this.state.search, 1);
+    }
+  }
+  onLoadMore = () => {
+    this.callServer(this.state.search, this.state.page + 1);
   };
 
   updateSearch = searchLine => {
     this.setState({ search: searchLine });
   };
+
+  showModal = item => {
+    this.setState(state => {
+      return {
+        ...state,
+        modalItem: item,
+      };
+    });
+  };
+  closeModal = () => {
+    this.setState(state => {
+      return {
+        ...state,
+        modalItem: null,
+      };
+    });
+  };
   render() {
     return (
       <>
         <SearchBar update={this.updateSearch} />
-        <ImageGallery search={this.state.search} />
+        <ImageGallery
+          search={this.state.search}
+          photos={this.state.photos}
+          onShowModal={this.showModal}
+        />
+        {this.state.page < this.state.pageCount ? (
+          <Button onLoadMore={this.onLoadMore} />
+        ) : (
+          ''
+        )}
+        {this.state.isEmpty ? 'Sorry,there are no photos available' : ''}
+        {this.state.modalItem && (
+          <Modal
+            item={this.state.modalItem}
+            closeModal={this.closeModal}
+          ></Modal>
+        )}
+        {this.state.isLoading && <Loader />}
       </>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,111 +1,23 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 import css from '../css/Styles.module.css';
-import { fetchPhotos } from '../../servise/image-servise';
+
 import { Component } from 'react';
-import { Button } from 'components/Button/Button';
-import { Modal } from '../Modal/Modal';
-import { Loader } from '../Loader/Loader';
 
 export class ImageGallery extends Component {
-  state = {
-    photos: [],
-    loading: true,
-    error: false,
-    page: 1,
-    modalItem: null,
-    isLoading: false,
-  };
-
-  callServer(searchLine, page) {
-    this.setState(state => ({
-      ...state,
-      isLoading: true,
-    }));
-
-    fetchPhotos(searchLine, page)
-      .then(result => {
-        // console.log('fetch', result, typeof result);
-        this.setState(state => ({
-          // photos: state.photos.concat(result.hits),
-          photos: [...state.photos, ...result.hits],
-          loading: false,
-          error: false,
-          isLoading: false,
-          page: page,
-        }));
-      })
-      .catch(error => {
-        console.log(error.message);
-        this.setState({ isLoading: false });
-      });
-  }
-  clearGallery() {
-    this.setState(state => ({
-      photos: [],
-      loading: false,
-      error: false,
-      page: 1,
-    }));
-  }
-
-  showModal = item => {
-    // console.log('show', item);
-    this.setState(state => {
-      return {
-        ...state,
-        modalItem: item,
-      };
-    });
-  };
-  closeModal = () => {
-    this.setState(state => {
-      return {
-        ...state,
-        modalItem: null,
-      };
-    });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    // console.log('didUpdate', this.props, this.state);
-    if (this.props.search !== prevProps.search) {
-      // console.log(this.props.search);
-      // console.log(prevProps.search);
-      this.clearGallery();
-      this.callServer(this.props.search, 1);
-    }
-  }
-  onLoadMore = () => {
-    this.callServer(this.props.search, this.state.page + 1);
-    // this.setState({ ...this.state, isLoading: true });
-  };
-
   render() {
-    if (this.state.photos.length) {
-      // console.log('render', this.state.photos);
-      return (
-        <>
-          <ul className={css.image_gallery}>
-            {this.state.photos.map(item => (
-              <ImageGalleryItem
-                key={item.id}
-                item={item}
-                onClick={this.showModal}
-              />
-            ))}
-          </ul>
-          <Button onLoadMore={this.onLoadMore} />
-          {this.state.modalItem && (
-            <Modal
-              item={this.state.modalItem}
-              closeModal={this.closeModal}
-            ></Modal>
-          )}
-          {this.state.isLoading && <Loader />}
-        </>
-      );
-    }
-    return this.state.isLoading && <Loader />;
+    return (
+      <>
+        <ul className={css.image_gallery}>
+          {this.props.photos.map(item => (
+            <ImageGalleryItem
+              key={item.id}
+              item={item}
+              onClick={this.props.onShowModal}
+            />
+          ))}
+        </ul>
+      </>
+    );
   }
 }
