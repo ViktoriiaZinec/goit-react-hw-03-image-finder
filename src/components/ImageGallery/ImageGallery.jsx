285b3ba060a71634494fab3068d001996c4a31cf
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-06-03 20:22:20 +0300

    Change class ImageGallery on function

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,18 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-
 import css from '../css/Styles.module.css';
 
-import { Component } from 'react';
-
-export class ImageGallery extends Component {
-  render() {
-    return (
-      <>
-        <ul className={css.image_gallery}>
-          {this.props.photos.map(item => (
-            <ImageGalleryItem
-              key={item.id}
-              item={item}
-              onClick={this.props.onShowModal}
-            />
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+export const ImageGallery = () => {
+  return (
+    <>
+      <ul className={css.image_gallery}>
+        {this.props.photos.map(item => (
+          <ImageGalleryItem
+            key={item.id}
+            item={item}
+            onClick={this.props.onShowModal}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
